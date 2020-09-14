8971e42204f08214cc66f00d24294fab3a3cfce7
gabe11 <graziano_gabriel@yahoo.com>
2020-09-14 16:21:12 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	Bla()
+
+}
+
+// Bla does bla
+
+func Bla() string {
+	return fmt.Sprintln("hello")
+}
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestBla(t *testing.T) {
+	if s := Bla(); s == "" {
+		t.Errorf("error: %v", s)
+	}
+
+}
